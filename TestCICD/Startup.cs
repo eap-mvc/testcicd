dab98b97fa8a959e961f3e8bd12355724e83c48c
Cris Dev Lap MSI <vva1994@gmail.com>
2022-06-25 16:18:21 +0700

    :)

---
+++ TestCICD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestCICD.Startup))]
+namespace TestCICD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
